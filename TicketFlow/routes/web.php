d58d40fac7afbd37dbc89c9bbad47292db4d3ee5
dounia-elg <dounia.elg20@gmail.com>
2025-02-26 20:26:39 +0000

    Define Routes

--- TicketFlow/routes/web.php
+++ TicketFlow/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\AssignmentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,9 +14,16 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Ticket routes
+    Route::resource('tickets', TicketController::class);
+
+    // Assignment routes
+    Route::resource('assignments', AssignmentController::class);
 });
 
 require __DIR__.'/auth.php';
