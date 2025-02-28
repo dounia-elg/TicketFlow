2aeb14159a5c266ea8b87df78d0b1b936e931349
dounia-elg <dounia.elg20@gmail.com>
2025-02-28 17:08:31 +0000

    Install Tailwind css

---
+++ TicketFlow/tailwind.config.js
@@ -0,0 +1,12 @@
+// filepath: /c:/Users/youco/Desktop/TicketFlow/TicketFlow/tailwind.config.js
+module.exports = {
+    content: [
+      './resources/**/*.blade.php',
+      './resources/**/*.js',
+      './resources/**/*.vue',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
